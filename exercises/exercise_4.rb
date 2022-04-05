bd9f0bef1c4b9775001b4bbe0accfea239e7affd
Josh Sarnecki <joshsarnecki@gmail.com>
2022-04-05 04:06:39 +0000

    Ex 6, added multiple employees to database

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -12,6 +12,7 @@ surrey.name = 'surrey'
 surrey.annual_revenue = 224000
 surrey.mens_apparel = false
 surrey.womens_apparel = true
+@store4 = surrey
 surrey.save
 
 whistler = Store.new
@@ -19,6 +20,7 @@ whistler.name = 'whistler'
 whistler.annual_revenue = 1900000
 whistler.mens_apparel = true
 whistler.womens_apparel = false
+@store5 = whistler
 whistler.save
 
 yaletown = Store.new
@@ -26,7 +28,14 @@ yaletown.name = 'yaletown'
 yaletown.annual_revenue = 430000
 yaletown.mens_apparel = true
 yaletown.womens_apparel = true
+@store6 = yaletown
 yaletown.save
+
+# puts @store1.name
+# puts @store2.name
+# puts @store3.name
+# puts @store4.name
+# puts @store5.name
 
 
 @mens_store = Store.where(mens_apparel: true)
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,43 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "James", last_name: "Bond", hourly_rate: 70)
+
+@store2.employees.create(first_name: "Brennan", last_name: "Craig", hourly_rate: 80)
+
+@store6.employees.create(first_name: "Jackie", last_name: "Pollack", hourly_rate: 60)
+
+@store4.employees.create(first_name: "Thanes", last_name: "Fiick", hourly_rate: 60)
+
+@store5.employees.create(first_name: "Kelsey", last_name: "Ray", hourly_rate: 90)
+
+@store1.employees.create(first_name: "Jes", last_name: "Opedd", hourly_rate: 65)
+
+@store2.employees.create(first_name: "Twang", last_name: "Xue", hourly_rate: 75)
+
+@store6.employees.create(first_name: "Collen", last_name: "Juck", hourly_rate: 60)
+
+@store4.employees.create(first_name: "Thomas", last_name: "MiddleDick", hourly_rate: 50)
+
+@store5.employees.create(first_name: "Ryyles", last_name: "Qwiky", hourly_rate: 85)
+
+@store1.employees.create(first_name: "Fres", last_name: "Werd", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Ian", last_name: "Efeng", hourly_rate: 90)
+
+@store6.employees.create(first_name: "Tyve", last_name: "Strak", hourly_rate: 60)
+
+@store4.employees.create(first_name: "Munnas", last_name: "Reqwald", hourly_rate: 75)
+
+@store5.employees.create(first_name: "Vusey", last_name: "Bopez", hourly_rate: 65)
+
+@store1.employees.create(first_name: "Yuxx", last_name: "Erwem", hourly_rate: 80)
+
+@store2.employees.create(first_name: "Logip", last_name: "e'Colle", hourly_rate: 75)
+
+@store6.employees.create(first_name: "Dyrun", last_name: "Juck", hourly_rate: 60)
+
+@store4.employees.create(first_name: "Ullquantayy", last_name: "Muck", hourly_rate: 95)
+
+@store5.employees.create(first_name: "Tus", last_name: "Zangg", hourly_rate: 85)

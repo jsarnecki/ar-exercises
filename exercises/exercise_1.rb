b4a522fa981626ebfb9e5d257f12aca6cd83b7c1
Josh Sarnecki <joshsarnecki@gmail.com>
2022-04-05 02:55:33 +0000

    First exercise

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,26 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+burnaby = Store.new
+burnaby.annual_revenue = 300000
+burnaby.name = 'burnaby'
+burnaby.mens_apparel = true
+burnaby.womens_apparel = true
+burnaby.save
+
+richmond = Store.new
+richmond.annual_revenue = 1260000
+richmond.name = 'richmond'
+richmond.mens_apparel = false
+richmond.womens_apparel = true
+richmond.save
+
+gastown = Store.new
+gastown.annual_revenue = 190000
+gastown.name = 'gastown'
+gastown.mens_apparel = true
+gastown.womens_apparel = false
+gastown.save
+
+puts Store.count
